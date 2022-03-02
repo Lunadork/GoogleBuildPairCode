fbabd59ca67fee922de4bc233f81cf86a8fed3d1
Zeiadork <zeiagillies@gmail.com>
2022-03-02 12:22:04 +0000

    Basic framework for buttons and results

--- server/controller/searchController.js
+++ server/controller/searchController.js
@@ -40,7 +40,7 @@ router.get('/random', (req,res) =>
 
 router.get('/', (req,res) =>
 {
-    res.send("Not yet implemented")
+   // res.sendFile("/client/index.html", { root: '..' });
 });
 
 module.exports = router;
