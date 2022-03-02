b77ffccba3ee7be81e3c6143a69babe8a14395e0
Zeiadork <zeiagillies@gmail.com>
2022-03-02 10:05:13 +0000

    Initial commits

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const server = express()
+const cors = require('cors');
+const bodyParser = require ('body-parser');
+const port = 3000;
+server.use(cors());
+
+const resultRoutes = require("./controller/searchController.js")
+server.use('/',resultRoutes)
+
